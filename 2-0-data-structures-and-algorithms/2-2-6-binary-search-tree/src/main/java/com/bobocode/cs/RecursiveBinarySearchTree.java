1dc1bf7e8635bb60b40557adcb39b79afad04add
yelyzavetalubenets <el1111zavetaaaaa@gmail.com>
2025-07-20 22:24:02 +0300

    YL-11 Add: Implementation to RecursiveBinarySearchTree insert + contains method - day 1

--- 2-0-data-structures-and-algorithms/2-2-6-binary-search-tree/src/main/java/com/bobocode/cs/RecursiveBinarySearchTree.java
+++ 2-0-data-structures-and-algorithms/2-2-6-binary-search-tree/src/main/java/com/bobocode/cs/RecursiveBinarySearchTree.java
@@ -2,6 +2,7 @@ package com.bobocode.cs;
 
 import com.bobocode.util.ExerciseNotCompletedException;
 
+import java.util.Arrays;
 import java.util.function.Consumer;
 
 /**
@@ -18,23 +19,84 @@ import java.util.function.Consumer;
  */
 public class RecursiveBinarySearchTree<T extends Comparable<T>> implements BinarySearchTree<T> {
 
+    private static class Node<T>{
+        T element;
+        Node<T> left;
+        Node<T> right;
+
+        public Node() {
+        }
+
+        public Node(T element) {
+            this.element = element;
+        }
+    }
+
+    private int size = 0;
+
+    private Node<T> root;
+
     public static <T extends Comparable<T>> RecursiveBinarySearchTree<T> of(T... elements) {
-        throw new ExerciseNotCompletedException();
+       RecursiveBinarySearchTree<T> newTree = new RecursiveBinarySearchTree<>();
+       Arrays.stream(elements).forEach(newTree::insert);
+       return newTree;
     }
 
     @Override
-    public boolean insert(T element) {
-        throw new ExerciseNotCompletedException();
+    public  boolean insert(T element) {
+        if (root == null) {
+            root = new Node<>(element);
+            size++;
+            return true;
+        } else return insertElement(element, root);
+    }
+
+    public boolean insertElement(T element, Node<T> root) {
+        T current = root.element;
+
+        if (element.compareTo(current) > 0) {
+            if (root.right == null) {
+                root.right = new Node<>(element);
+                size++;
+                return true;
+            } else return insertElement(element, root.right);
+        } else if (element.compareTo(current) < 0) {
+            if (root.left == null) {
+                root.left = new Node<>(element);
+                size++;
+                return true;
+            } else return insertElement(element, root.left);
+        } else
+            return false;
     }
 
     @Override
     public boolean contains(T element) {
-        throw new ExerciseNotCompletedException();
+        if(element == null){
+            throw new NullPointerException();
+        }
+        if(root == null){
+            return false;
+        } else return contains(root, element);
+    }
+
+    public boolean contains(Node<T> node, T element){
+        T current = node.element;
+
+        if(element.compareTo(current) > 0){
+            if(node.right == null){
+                return node.element == element;
+            } else return contains(node.right, element);
+        } else if(element.compareTo(current) < 0){
+            if(node.left == null){
+                return node.element == element;
+            } else return contains(node.left, element);
+        } else return true;
     }
 
     @Override
     public int size() {
-        throw new ExerciseNotCompletedException();
+        return size;
     }
 
     @Override
